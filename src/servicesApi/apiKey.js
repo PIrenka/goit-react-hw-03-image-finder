61cb939a2620e5acdc4c30a0aa566668456b3dfb
Iryna Protsenko <irene.protsenko@gmail.com>
2021-04-15 02:37:09 +0300

    fix hide api

---
+++ src/servicesApi/apiKey.js
@@ -0,0 +1,2 @@
+const API_KEY = '20341513-7d48bbe81977c2377736c4eb0';
+export default API_KEY;
--- src/servicesApi/images-api.js
+++ src/servicesApi/images-api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
+import API_KEY from './apiKey';
 // axios.defaults.baseURL = 'https://pixabay.com';
-const API_KEY = '20341513-7d48bbe81977c2377736c4eb0';
 const fetchImages = ({ searchQuery, currentPage }) => {
   return axios
     .get(
